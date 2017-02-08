5a7d6dcd8a69502bcdb7a5ca4fc2cb990d2fb5ad
John Clark <socrates37@gmail.com>
2017-02-08 06:03:29 -0700

    initial commit

---
+++ Treehub/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Treehub
+//
+//  Created by John M Clark on 2/7/17.
+//  Copyright © 2017 Treehub LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.green
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
