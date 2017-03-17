2b44a83502e24bf346248e21fcddc4fc2f09de72
John Clark <socrates37@gmail.com>
2017-03-17 12:03:53 -0600

    prep for default package install

--- Treehub/ViewController.swift
+++ Treehub/ViewController.swift
@@ -8,30 +8,64 @@
 
 import UIKit
 import WebKit
+import Zip
 
 class ViewController: UIViewController {
 
     var webView: WKWebView!
     
+    var defaultPackages: [String] = ["app", "test", "package-manager"]
+    
     override func loadView() {
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
-//        webView.uiDelegate = self
         view = webView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var content = "";
-        let baseURL = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        do {
-            let bundle = Bundle.main
-            let path = bundle.path(forResource: "index", ofType: "html")
-            content = try String(contentsOfFile: path!)
-        } catch {
-            
+        
+        // Ensure Library files exist
+        let libraryURI = try! FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        
+        // Ensure webview directory exists
+        let webviewURI = libraryURI.appendingPathComponent("webview", isDirectory: true)
+        try? FileManager.default.createDirectory(atPath: webviewURI.path, withIntermediateDirectories: true, attributes: nil)
+
+        // Ensure webview files exist
+        let webviewIndexDest = webviewURI.appendingPathComponent("index.html")
+        let webviewIndexSource = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "files/webview")!
+        if (!FileManager.default.fileExists(atPath: webviewIndexDest.path)) {
+            try! FileManager.default.copyItem(atPath: webviewIndexSource.path, toPath: webviewIndexDest.path)
         }
-        webView.loadHTMLString(content, baseURL: baseURL)
+        
+        // Ensure server directory exists
+        let serverURI = libraryURI.appendingPathComponent("server", isDirectory: true)
+        try? FileManager.default.createDirectory(atPath: serverURI.path, withIntermediateDirectories: true, attributes: nil)
+        
+        // TODO Ensure server files exist
+        
+        // Ensure packages directory exists
+        let packagesURI = libraryURI.appendingPathComponent("packages", isDirectory: true)
+        try? FileManager.default.createDirectory(atPath: packagesURI.path, withIntermediateDirectories: true, attributes: nil)
+
+        
+        // TODO Ensure default packages exist
+        let packages = installedPackages(path: packagesURI.path)
+        
+        for package in defaultPackages {
+            if (!packages.contains(package)) {
+                installPackage(package: package)
+            }
+        }
+        
+        // TODO create package.json
+        
+        // TODO Start Server
+        
+        // TODO Load WebView
+        let content = try! String(contentsOfFile: webviewIndexDest.path)
+        webView.loadHTMLString(content, baseURL: packagesURI)
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,5 +74,19 @@ class ViewController: UIViewController {
     }
 
 
+//    private func ensureDir(dir: String) {
+//        let libraryURI = try! FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+//        let uri = libraryURI.appendingPathComponent(dir, isDirectory: true)
+//        try? FileManager.default.createDirectory(atPath: uri.path, withIntermediateDirectories: true, attributes: nil)
+//    }
+    
+    private func installedPackages(path: String) -> [String] {
+        let paths = try! FileManager.default.contentsOfDirectory(atPath: path)
+        return paths.map { aContent in (path as NSString).appendingPathComponent(aContent)}
+    }
+    
+    private func installPackage(package: String, version: String = "latest") {
+        print("installing", package, version)
+    }
 }
 
