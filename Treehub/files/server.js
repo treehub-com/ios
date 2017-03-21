49bb5cb8744c27e29becdff306271efa3a6407ad
John Clark <socrates37@gmail.com>
2017-03-21 14:56:36 -0600

    prep for loading routes

---
+++ Treehub/files/server.js
@@ -0,0 +1,108 @@
+const files = {};
+const routes = {
+  _: coreRoute,
+};
+
+function addRoute(pkg, file) {
+  files[pkg] = file;
+}
+
+async function loadRoutes() {
+  // Loop through route files & load
+  for (const pkg of Object.keys(files)) {
+    const code = files[pkg];
+    const exported = {};
+    try {
+      eval(`((module) => {${code}})(exported)`);
+    } catch (error) {
+      console.error(`Error loading route for ${pkg}`);
+      console.error(error);
+      return;
+    }
+    if (typeof exported.exports !== 'function') {
+      console.error(`Could not load route for ${pkg}`);
+      return;
+    }
+    routes[pkg] = await exported.exports({
+      LevelUpBackend: LevelJS,
+      pathPrefix: pkg + '/',
+    });
+  }
+  window.webkit.messageHandlers.loaded.postMessage("");
+}
+
+/*
+response obj fields
+{
+  id, // passed in value
+  status, // Code
+  body, // String
+}
+*/
+function response(obj) {
+  window.webkit.messageHandlers.response.postMessage(JSON.stringify(obj));
+}
+
+function request({id, route, body}) {
+  const path = route.split('/').filter((s)=>s !== '');
+
+  if (path.length === 0) {
+    return response({
+      id: request.id,
+      status: 404,
+      body: JSON.stringify({message: 'No Route Specified'}),
+    });
+  }
+
+  const pkg = path.shift().toLowerCase();
+  if (routes[pkg] === undefined) {
+    return response({
+      id: request.id,
+      status: 404,
+      body: JSON.stringify({message: 'No Route for Package'}),
+    });
+  }
+
+  routes[pkg]({
+      route: `/${path.join('/')}`,
+      body: (body) ? JSON.parse(body) : {},
+    })
+    .then((body) => {
+      response({
+        id,
+        status: 200,
+        body: JSON.stringify(body),
+      });
+    })
+    .catch((error) => {
+      response({
+        id,
+        status: error.status || 500,
+        body: JSON.stringify({message: error.message}),
+      });
+    })
+}
+
+async function coreRoute({route, body}) {
+  switch(route) {
+    case '/package/install':
+      return installPackage(body);
+    case '/package/uninstall':
+      return uninstallPackage(body);
+    default:
+      const error = new Error('Unknown Route');
+      error.status = 404;
+      throw error;
+  }
+}
+
+async function installPackage({name, version = 'latest'}) {
+  console.log(`installing package ${name}`);
+  // TODO
+  return true;
+};
+
+async function uninstallPackage({name}) {
+  console.log(`uninstalling package ${name}`);
+  return true;
+};
