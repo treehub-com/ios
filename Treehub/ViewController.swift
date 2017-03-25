0123a08101ff243d37d43082ebca35e2fc959b89
John Clark <socrates37@gmail.com>
2017-03-25 09:25:53 -0600

    switched over to PromiseKit

--- Treehub/ViewController.swift
+++ Treehub/ViewController.swift
@@ -8,6 +8,7 @@
 
 import Alamofire
 import GCDWebServers
+import PromiseKit
 import SwiftyJSON
 import UIKit
 import WebKit
@@ -15,9 +16,10 @@ import Zip
 
 class ViewController: UIViewController, WKScriptMessageHandler {
 
-    var webView: WKWebView!
+    var webview: WKWebView!
     var webserver: GCDWebServer!
     var server: WKWebView! // TODO replace this with JavascriptCore
+    var serverLoaded: ((_ error: Error?) -> Void)! // Called via server.js after routes are loaded to resolve a promise
 
     var requests: [String: GCDWebServerCompletionBlock] = [:]
 
@@ -25,104 +27,80 @@ class ViewController: UIViewController, WKScriptMessageHandler {
     var packagesURL: URL!
     
     var defaultPackages: [String] = ["app", "api", "package-manager"]
-    var installingPackages: [String: Bool] = [:]
-    var packagesJSON: JSON = JSON(data: "{}".data(using: .utf8)!)
-    
+
     override func loadView() {
         // Set URLs
         let libraryURL = try! FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        self.filesURL = libraryURL.appendingPathComponent("files", isDirectory: true)
+        self.filesURL = Bundle.main.bundleURL.appendingPathComponent("files", isDirectory: true)
         self.packagesURL = libraryURL.appendingPathComponent("packages", isDirectory: true)
 
         // Set webview
-        self.webView = WKWebView(frame: .zero, configuration: WKWebViewConfiguration())
-        self.view = webView
+        self.webview = WKWebView(frame: .zero, configuration: WKWebViewConfiguration())
+        self.view = webview
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.ensureDirectories()
-        self.installDefaultPackages() // Will call defaultPackagesInstalled() after packages are installed
+
         self.startWebserver()
-    }
-
-    private func defaultPackagesInstalled() {
-        self.createPackagesJSON()
-        self.startServer() // Will call loadWebview()
-    }
-
-
-    private func ensureDirectories() {
-        // Ensure html directory exists/populated
-        try? FileManager.default.createDirectory(atPath: self.filesURL.path, withIntermediateDirectories: true, attributes: nil)
-        let files = try! FileManager.default.contentsOfDirectory(atPath: Bundle.main.bundleURL.appendingPathComponent("files", isDirectory: true).path)
-
-        for file in files {
-            let source = Bundle.main.bundleURL.appendingPathComponent("files/" + file)
-            let dest = self.filesURL.appendingPathComponent(file)
-            // TODO don't always overwrite
-            if (FileManager.default.fileExists(atPath: dest.path)) {
-                try! FileManager.default.removeItem(at: dest)
-            }
-            try! FileManager.default.copyItem(atPath: source.path, toPath: dest.path)
-        }
-
-        // Ensure packages directory exists
-        try? FileManager.default.createDirectory(atPath: packagesURL.path, withIntermediateDirectories: true, attributes: nil)
+        self.webview.load(URLRequest(url: URL(string:"http://localhost:8985")!))
         
-    }
-
-    private func installDefaultPackages() {
-        let packages = try! FileManager.default.contentsOfDirectory(atPath: self.packagesURL.path)
-
-        for package in self.defaultPackages {
-            if (!packages.contains(package)) {
-                self.installingPackages[package] = true
-                self.installPackage(package: package)
+        
+        self.ensureDefaultPackages()
+            .then { _ in
+                return self.startServer()
             }
-        }
-        if (self.installingPackages.count == 0) {
-            self.defaultPackagesInstalled();
-        }
+            .then { _ in
+                self.webview.evaluateJavaScript("window._load()", completionHandler: nil)
+            }
+            .catch { error in
+                print(error)
+            }
+
     }
 
     private func startWebserver() {
         // Load WebServer
-        webserver = GCDWebServer()
+        self.webserver = GCDWebServer()
 
         // Note that Routes are evaluated last to first
 
         // Default is to load index.html
-        webserver.addDefaultHandler(forMethod: "GET", request: GCDWebServerRequest.self, processBlock: {request in
+        self.webserver.addDefaultHandler(forMethod: "GET", request: GCDWebServerRequest.self, processBlock: {request in
             let content = try! String(contentsOfFile: self.filesURL.appendingPathComponent("index.html").path)
             return  GCDWebServerDataResponse(data: content.data(using: .utf8), contentType: "text/html")
         })
         // Any .js file gets pulled from packages dir
-        webserver.addHandler(forMethod: "GET", pathRegex: "/.*\\.js", request: GCDWebServerRequest.self, processBlock: {request in
+        self.webserver.addHandler(forMethod: "GET", pathRegex: "/.*\\.js", request: GCDWebServerRequest.self, processBlock: {request in
             let content = try! String(contentsOfFile: self.packagesURL.appendingPathComponent("." + (request?.path)!).path)
             return  GCDWebServerDataResponse(data: content.data(using: .utf8), contentType: "application/javascript")
         })
         // packages.json
-        webserver.addHandler(forMethod: "GET", path: "/packages.json", request: GCDWebServerRequest.self, processBlock: {request in
-            return  GCDWebServerDataResponse(data: self.packagesJSON.rawString()?.data(using: .utf8), contentType: "application/json")
+        self.webserver.addHandler(forMethod: "GET", path: "/packages.json", request: GCDWebServerRequest.self, processBlock: {request in
+            return  GCDWebServerDataResponse(data: self.getPackagesJSON().rawString()?.data(using: .utf8), contentType: "application/json")
         })
         // _ files
-        webserver.addHandler(forMethod: "GET", pathRegex: "/_/.*", request: GCDWebServerRequest.self, processBlock: {request in
+        self.webserver.addHandler(forMethod: "GET", pathRegex: "/_/.*", request: GCDWebServerRequest.self, processBlock: {request in
             let path = request?.path.replacingOccurrences(of: "/_/", with: "")
             let content = try! String(contentsOfFile: self.filesURL.appendingPathComponent(path!).path)
             return  GCDWebServerDataResponse(data: content.data(using: .utf8), contentType: "text/html")
         })
         // POST requests
-        webserver.addDefaultHandler(forMethod: "POST", request: GCDWebServerDataRequest.self, asyncProcessBlock: { request, completionBlock in
+        self.webserver.addDefaultHandler(forMethod: "POST", request: GCDWebServerDataRequest.self, asyncProcessBlock: { request, completionBlock in
             // TODO handle internal routes in swift
             let path = (request?.path)!
 
             if path.hasPrefix("/_/") {
                 if path == "/_/package/install" {
-//                    let body = JSON((request as! GCDWebServerDataRequest).data)
-//                    self.installPackage(package: body["name"].stringValue)
-                    completionBlock!(GCDWebServerDataResponse(data: "true".data(using: .utf8), contentType: "application/json"))
+                    let body = JSON((request as! GCDWebServerDataRequest).data)
+                    self.installPackage(package: body["name"].stringValue)
+                        .then { _ in
+                            completionBlock!(GCDWebServerDataResponse(data: "true".data(using: .utf8), contentType: "application/json"))
+                        }
+                        .catch { error in
+                            print(error)
+                        }
+                    return
                 } else if path == "/_/package/uninstall" {
                     completionBlock!(GCDWebServerDataResponse(data: "true".data(using: .utf8), contentType: "application/json"))
                 } else {
@@ -145,24 +123,15 @@ class ViewController: UIViewController, WKScriptMessageHandler {
             }
         })
 
-        webserver.start(withPort: 8985, bonjourName: "Treehub")
+        self.webserver.start(withPort: 8985, bonjourName: "Treehub")
 
     }
 
-    private func createPackagesJSON() {
-        let installedPackages = try! FileManager.default.contentsOfDirectory(atPath: packagesURL.path)
-
-        for package in installedPackages {
-            let json = try! String(contentsOfFile: packagesURL.appendingPathComponent(package + "/treehub.json").path)
-            self.packagesJSON[package] = JSON(data: json.data(using: .utf8, allowLossyConversion: false)!)
-        }
-    }
-
-    private func startServer() {
+    private func startServer() -> Promise<Void> {
         // Load routes
         // TODO do this a better way
         var routes = JSON(data: "{}".data(using: .utf8)!)
-        for (package, json) in self.packagesJSON {
+        for (package, json) in self.getPackagesJSON() {
             if let path = json["route"].string {
                 // TODO catch this error
                 let packagePath = self.packagesURL.appendingPathComponent(package)
@@ -183,17 +152,27 @@ class ViewController: UIViewController, WKScriptMessageHandler {
         self.server = WKWebView(frame: .zero, configuration: config)
 
         self.server.load(URLRequest(url: URL(string:"http://localhost:8985/_/server.html")!)) // Will call loadWebview() after loading routes
+
+        return Promise { fulfill, reject in
+            self.serverLoaded = { (_ error: Error?) -> Void in
+                if error == nil {
+                    fulfill()
+                } else {
+                    reject(error!)
+                }
+            }
+        }
     }
 
     private func loadWebview() {
-        self.webView.load(URLRequest(url: URL(string:"http://localhost:8985")!))
+        self.webview.load(URLRequest(url: URL(string:"http://localhost:8985")!))
     }
 
     /* Javascript Callback Functions */
 
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         if (message.name == "loaded") {
-            self.loadWebview()
+            self.serverLoaded(nil)
         }
         if (message.name == "response") {
             let rawResponse = JSON(data: (message.body as! String).data(using: .utf8)!)
@@ -209,7 +188,30 @@ class ViewController: UIViewController, WKScriptMessageHandler {
 
     /* Utility Functions */
 
-    private func installPackage(package: String, version: String = "latest") {
+    private func ensureDefaultPackages() -> Promise<Void> {
+        // Ensure packages directory exists
+        try? FileManager.default.createDirectory(atPath: packagesURL.path, withIntermediateDirectories: true, attributes: nil)
+
+        // Get installed packages
+        let packages = try! FileManager.default.contentsOfDirectory(atPath: self.packagesURL.path)
+
+        var promises: [Promise<Void>] = []
+
+        for package in self.defaultPackages {
+            if (!packages.contains(package)) {
+                promises.append(self.installPackage(package: package))
+            }
+        }
+        if (promises.count > 0) {
+            return join(promises)
+        }
+
+        return Promise { fulfill, reject in
+            fulfill()
+        }
+    }
+
+    private func installPackage(package: String, version: String = "latest") -> Promise<Void> {
         
         let url = URL(string: "https://packages.treehub.com/" + package + "/" + version + ".zip")
         
@@ -222,31 +224,33 @@ class ViewController: UIViewController, WKScriptMessageHandler {
         
         let destinationURL = self.packagesURL.appendingPathComponent(package, isDirectory: true)
         try? FileManager.default.createDirectory(atPath: destinationURL.path, withIntermediateDirectories: true, attributes: nil)
-        
-        Alamofire.download(url!, to: cacheDestination).response { response in
-            if response.error == nil {
-                self.unzipPackage(source: response.destinationURL!, destination: destinationURL)
-            } else {
-                // TODO record error
-            }
-            self.installingPackages[package] = false
 
-            // If we are the last to finish, call defaultPackagesInstalled()
-            for (_,value) in self.installingPackages {
-                if (value == true) {
-                    return;
+        return Promise { fulfill, reject in
+            Alamofire.download(url!, to: cacheDestination).response { response in
+                if response.error == nil {
+                    do {
+                        try Zip.unzipFile(response.destinationURL!, destination: destinationURL, overwrite: true, password: nil, progress: nil)
+                        fulfill()
+                    } catch {
+                        reject(error)
+                    }
+                } else {
+                    reject(response.error!)
                 }
             }
-            self.defaultPackagesInstalled()
         }
     }
-    
-    private func unzipPackage(source: URL, destination: URL) {
-        do {
-            try Zip.unzipFile(source, destination: destination, overwrite: true, password: nil, progress: nil)
-        } catch {
-            print(error)
+
+    private func getPackagesJSON() -> JSON {
+        let installedPackages = try! FileManager.default.contentsOfDirectory(atPath: packagesURL.path)
+
+        var packagesJSON: JSON = JSON(data: "{}".data(using: .utf8)!)
+
+        for package in installedPackages {
+            let json = try! String(contentsOfFile: self.packagesURL.appendingPathComponent(package + "/treehub.json").path)
+            packagesJSON[package] = JSON(data: json.data(using: .utf8, allowLossyConversion: false)!)
         }
+        return packagesJSON
     }
 }
 
